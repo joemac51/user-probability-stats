86ff27b544d7f321b0d204e9b59697c8dd57c80a
Daniel <daniel.horrigan@live.ie>
2020-03-09 09:57:25 +0000

    initial commit

---
+++ auto_user_events.py
@@ -0,0 +1,3 @@
+# Make lists of eventTarget, eventType, and entityType
+# Randomly pick from each list (.choice() method) and construct a dict of them
+# use requests to send a put request to UserEvent api
---
+++ prob_plot.py
@@ -0,0 +1,40 @@
+import requests
+import matplotlib.pyplot as plt
+import numpy as np
+
+def get_json(user_id: str) -> list:
+    '''Returns a list of dicts containing TMSID and their corresponding probabilities
+    for the user's top 1000 of titles'''
+
+    r = requests.get(f"http://movie-demo-daniel.davidmcgettigan.com/getlistforusernormnew/{user_id}")
+
+    return r.json()
+
+prob_dict_list = get_json("ed05c06f79baa07b")
+
+def probs_to_arr(a_list: list) -> np.ndarray:
+    '''Appends the probabilities to a numpy array'''
+
+    arr = np.array([])
+
+    for i in a_list:
+        arr = np.append(arr, i["weight"])
+
+    return arr
+
+probabilities = probs_to_arr(prob_dict_list)
+
+def plot_probs(arr: np.ndarray) -> plt.figure:
+    '''Plots the number of titles (x-axis) vs. the probabilities (y-axis)'''
+
+    title_num = np.arange(arr.shape[0])
+
+    plt.figure()
+    plt.title(r'Title Probabilities vs. Number of Titles')
+    plt.xlabel(r'Title No.')
+    plt.ylabel(r'Probabilities')
+    plt.plot(title_num, arr, ls='', marker='o', ms=0.5)
+    plt.show()
+
+if __name__ == "__main__":
+    plot_probs(probabilities)
